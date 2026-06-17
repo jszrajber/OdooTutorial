638b4e7997afa2d096cbc1c27655ab4a4725cbc4
kuba <jakubszrajber12@gmail.com>
2026-06-17 16:27:21 +0200

    WIP: Creating UI

--- addons/hello/__manifest__.py
+++ addons/hello/__manifest__.py
@@ -2,7 +2,9 @@
     "name": 'Hello',
     'version': '1.0',
     'depends': ['base'],    # Which modules must be installed before mine
-    'data': [],
+    'data': [
+        'views/product_views.xml'
+        ],
     'installable': True,
 }
 
